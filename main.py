bb80d9431659a0d686dc4111d507c2b3e4b3ea06
Tania Curutchi <tania.curutchi@gmail.com>
2023-04-12 23:41:02 +0300

    Proof of concept

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
+import recastdetour as rd
+import json
 from flask import Flask
+
 app = Flask(__name__)
 
 
+def convert_point(point):
+    point = list(map(lambda p: p / 1000 / 111, point))
+    return [point[0] + 45.74648651703219, 0, point[2] + 21.22946578527367]
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def get_path():
+    navmesh = rd.Navmesh()
+    navmesh.load_navmesh("Data/navmesh-test2.bin")
+    start = (-3.3, 0, -10.23)
+    end = (-3.4, 4.5, 18.31)
+    path = navmesh.pathfind_straight(start, end)
+    path = list(map(lambda p: convert_point(p), path))
+    json_response = json.dumps(path)
+    print(json_response)
+    return json_response
 
 
 if __name__ == '__main__':
